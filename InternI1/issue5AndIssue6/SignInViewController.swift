efdcae540a51495dac338f9d5c087277890b48f2
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2025-03-17 23:33:59 +0700

    - issue_5 and issue_6: practice auto layout and stack view

---
+++ InternI1/issue5AndIssue6/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  InternI1
+//
+//  Created by Thạnh Dương Hoàng on 17/3/25.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
