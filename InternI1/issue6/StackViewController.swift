eae05c72b1924ac880a330f72bd22d7a101a23a3
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2025-03-19 16:57:06 +0700

    - issue 6: practice stack view and use spacer view for auto layout

---
+++ InternI1/issue6/StackViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StackViewController.swift
+//  InternI1
+//
+//  Created by Thạnh Dương Hoàng on 19/3/25.
+//
+
+import UIKit
+
+class StackViewController: UIViewController {
+    
+}
---
+++ InternI1/issue6/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  InternI1
+//
+//  Created by Thạnh Dương Hoàng on 19/3/25.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
